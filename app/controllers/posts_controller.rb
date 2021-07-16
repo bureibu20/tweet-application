552ed21b73ca56a280b54c1d5239608dc690fb1f
kenya ohara <bureibu20@gmail.com>
2021-07-16 18:45:56 +0900

    fix3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,14 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "気持ちを編集しました！"
+    else
+      render :edit
+    end
+  end
   private
   def post_params
     params.require(:post).permit(:name, :content)
