e7a59fdae79e0e15cdfe7c2dc053134c0239b859
kenya ohara <bureibu20@gmail.com>
2021-07-16 17:36:29 +0900

    fix2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,23 @@
 class PostsController < ApplicationController
   def index
-      
+    @posts = Post.all
   end
   def new
       @post = Post.new
   end
   def create
-      Post.create(content: params[:post][:content])
+    @post =Post.create(post_params)
+    if @post.save
+      redirect_to new_post_path, notice: "気持ちをTweetしました！"
+    else
+      render :new
+    end
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  private
+  def post_params
+    params.require(:post).permit(:name, :content)
   end
 end
-
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :content, presence: true
+  validates :name, presence: true
+  validates :content, presence: true, length: { in: 1..140 }
+  validates :content, length: { maximum: 140 }
 end
