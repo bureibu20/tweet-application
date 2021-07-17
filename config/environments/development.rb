c96930789a957297125afeb41ebd40d4c334ad24
kenya ohara <bureibu20@gmail.com>
2021-07-17 21:43:43 +0900

    コメント修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,20 +4,22 @@ Rails.application.configure do
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
-
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
+
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      Cache-Control: "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
+
     config.cache_store = :null_store
   end
   config.active_storage.service = :local
   config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_support.disallowed_deprecation = :raise
@@ -27,5 +29,4 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.quiet = true
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
 end
